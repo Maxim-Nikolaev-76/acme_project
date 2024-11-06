fc48c56816d72747fe7c535abefdc20bb46468a3
Maxim <zlodey000@mail.ru>
2024-11-06 22:59:39 +0500

    make auth users

--- acme_project/acme_project/settings.py
+++ acme_project/acme_project/settings.py
@@ -88,4 +88,13 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 
+# Подключаем бэкенд filebased.EmailBackend:
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+# Указываем директорию, в которую будут сохраняться файлы писем:
+EMAIL_FILE_PATH = BASE_DIR / 'sent_emails'
+
+LOGIN_REDIRECT_URL = 'pages:homepage'
+
+LOGIN_URL = 'login'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
